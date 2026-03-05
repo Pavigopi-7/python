a1b7c13578f58f6404d8e770c84b205898fba1f9
Pavigopi-7 <95678553+Pavigopi-7@users.noreply.github.com>
2026-03-05 12:17:24 +0530

    Add files via upload

---
+++ simpleinterest.py
@@ -0,0 +1,10 @@
+p = int(input("principle amount is :"))
+print("p is :",p)
+n = int(input("number of days is :"))
+print("n is :",n)
+r = int(input("rate of interest"))
+print("r is :",r)
+si =p*n*r/100
+print("simple Interest is :",si)
+tm = si*p
+print("total amount is :",tm)
---
+++ swap.py
@@ -0,0 +1,7 @@
+num1=int(input("enter a number :"))
+print("number1 is:",num1)
+num2 = int(input("enter a number :"))
+print("number2 is:",num2)
+num1,num2 =num2,num1
+print("swap number is :", num1,num2)
+
