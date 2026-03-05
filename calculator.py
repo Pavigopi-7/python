47864146b43a052c7b41f15072c84557d86f9616
Pavigopi-7 <95678553+Pavigopi-7@users.noreply.github.com>
2026-03-05 12:19:25 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,40 @@
+a = int(input("enter a number :"))
+print("no.is :",a)
+b = int(input("enter a number :"))
+print("no is :",b)
+c = input("add")
+add = a+b
+print("addition is :",add)
+d =input("sub")
+sub = a-b
+print("subraction is :",sub)
+e = input("mul")
+mul = a*b
+print("multiplication is :",mul)
+f = input("div")
+div = a/b
+print("division is :",div)
+g = input("mod")
+mod =a%b
+print("modulous is :",mod)
+h = input("powerof")
+pow = a^b
+print("power is :",pow)
+
+
+# This is aa sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
