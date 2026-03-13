dc86db35e427c745c97577251b34b5de922931e1
Pavigopi-7 <95678553+Pavigopi-7@users.noreply.github.com>
2026-03-13 17:02:01 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,19 @@
+str = input("enter a string1:")
+print(str)
+print(len(str))
+print(str[0])
+print(str[:4])
+print(str[5:])
+print(str.lower())
+print(str.upper())
+print(str.count(str))
+str2 = input("enter a string:")
+print(str2)
+str3 = str.replace(str,str2)
+print(str3)
+str4 = str+str2
+print(str4)
+string = f'{str},{str2}.hai@'
+print(string)
+
+
