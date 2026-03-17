259b0cf5f19e633215fb6c84df6273089edb5920
Pavigopi-7 <95678553+Pavigopi-7@users.noreply.github.com>
2026-03-17 17:18:51 +0530

    Add files via upload

---
+++ studclassinit.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self):
+        self.name = input("enter your name:")
+        self.id = int(input("enter your id number:"))
+        self.mark = int(input("enter your marks obtained:"))
+
+    def display(self):
+            print("Name:", self.name)
+            print("ID number:", self.id)
+            print("Marks obtained:", self.mark)
+stu = Student()
+stu.display()
